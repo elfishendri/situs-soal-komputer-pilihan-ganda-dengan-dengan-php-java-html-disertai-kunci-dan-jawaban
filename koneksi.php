e1160c56d1edd53f3f1b3431aa608028a050aa0d
Elfis Hendri, S.Pd, CH, CHt, CI, CGra, CBek, CAkp <74452353+elfishendri@users.noreply.github.com>
2020-12-25 20:13:28 +0700

    situs soal pilihan ganda
    situs-soal-komputer-pilihan-ganda-dengan-dengan-php-java-html-disertai-kunci-dan-jawaban

---
+++ koneksi.php
@@ -0,0 +1,11 @@
+<?php 
+// $host="localhost";
+// $user="root";
+// $pass="" ;
+// $dbnama="db_tutorial";
+// mysql_connect($host, $user, $pass) or die ("Database tidak dapat di akses...!");
+// mysql_select_db($dbnama);
+
+$conn = mysqli_connect("localhost","root","", "db_tutorial") or die ("Database tidak dapat di akses...!");
+
+ ?>
